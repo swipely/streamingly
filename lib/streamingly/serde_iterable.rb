5c83518f3599ed0a8817c2e324fa02b0af233625
Peak Xu <peak.xu@gmail.com>
2016-01-25 15:14:50 -0500

    Support passing in an error handler in case SerDe fails

--- lib/streamingly/serde_iterable.rb
+++ lib/streamingly/serde_iterable.rb
@@ -1,13 +1,24 @@
 module Streamingly
 
   class SerDeIterable
-    def initialize(iterable)
+    def initialize(iterable, error_handler = nil)
       @iterable = iterable
+      @error_handler = error_handler
+      @error_callback_defined = @error_handler &&
+                                @error_handler.method_defined?(:on_error)
     end
 
     def each
       @iterable.each do |line|
-        yield Streamingly::SerDe.from_tabbed_csv(line)
+        begin
+          yield Streamingly::SerDe.from_tabbed_csv(line)
+        rescue => error
+          if @error_callback_defined
+            @error_handler.send(:on_error, error, line: line)
+          else
+            raise error
+          end
+        end
       end
     end
   end
